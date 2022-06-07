8d8cb038132e47f9fa4045ec21734359113271c0
ozgeAtik <ozgeatik4@gmail.com>
2022-06-07 21:50:58 +0300

    fixed maps

--- app/src/main/java/com/example/reheimer2/MapsActivity.java
+++ app/src/main/java/com/example/reheimer2/MapsActivity.java
@@ -116,15 +116,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        /* In order to access user's location, we need to ask for permission. If those permissions
+         *  are granted then we can access latitude and longitude of the user. */
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    /* For close location we need to ask for ACCESS_FINE_LOCATION. But we can not
+                     *  access ACCESS_FINE_LOCATION without also asking for ACCESS_COARSE_LOCATION
+                     *  permission */
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},100);
+        }
+
+        /* When location changes, to keep up with it we can use onLocationChanged method.
+         *  Gets the new location and adds the marker. */
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                LatLng lct = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(lct).title("Your location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(lct));
+            }
+        });
+    }
+
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -136,11 +160,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},100);
         }
-
-        // Add a marker in Turkey and move the camera
-        LatLng turkey = new LatLng(41, 28);
-        mMap.addMarker(new MarkerOptions().position(turkey).title("Marker in Turkey"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(turkey));
         mMap.setMyLocationEnabled(true);
     }
 }
